78b3c6bbf21e44db701fba6f479ecc78c159df00
mlmpk <mlempik@gmail.com>
2014-12-15 21:55:13 +0100

    cleared

--- Aufgabenblatt5/src/aufgabe1/Login.java
+++ Aufgabenblatt5/src/aufgabe1/Login.java
@@ -6,7 +6,6 @@ import java.util.UUID;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
